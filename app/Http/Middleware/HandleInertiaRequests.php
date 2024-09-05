ccf2f5e93a692aea1927565443a670767e1d5631
Stas Markov <stas.markov1995@gmail.com>
2024-09-05 10:31:33 +0300

    Moved menu to the own compoonent file

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -6,39 +6,38 @@ use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
 
-class HandleInertiaRequests extends Middleware
-{
-    /**
-     * The root template that is loaded on the first page visit.
-     *
-     * @var string
-     */
-    protected $rootView = 'app';
+class HandleInertiaRequests extends Middleware {
 
-    /**
-     * Determine the current asset version.
-     */
-    public function version(Request $request): string|null
-    {
-        return parent::version($request);
-    }
+  /**
+   * The root template that is loaded on the first page visit.
+   *
+   * @var string
+   */
+  protected $rootView = 'app';
 
-    /**
-     * Define the props that are shared by default.
-     *
-     * @return array<string, mixed>
-     */
-    public function share(Request $request): array
-    {
-        return [
-            ...parent::share($request),
-            'auth' => [
-                'user' => $request->user(),
-            ],
-            'ziggy' => fn () => [
-                ...(new Ziggy)->toArray(),
-                'location' => $request->url(),
-            ],
-        ];
-    }
+  /**
+   * Determine the current asset version.
+   */
+  public function version(Request $request): string|null {
+    return parent::version($request);
+  }
+
+  /**
+   * Define the props that are shared by default.
+   *
+   * @return array<string, mixed>
+   */
+  public function share(Request $request): array {
+    return [
+      ...parent::share($request),
+      'auth' => [
+        'user' => $request->user(),
+      ],
+      'ziggy' => fn() => [
+        ...(new Ziggy)->toArray(),
+        'location' => $request->url(),
+      ],
+    ];
+  }
+
 }
---
+++ resources/js/Components/Menus/MainMenu.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import {Link} from "@inertiajs/vue3";
+</script>
+
+<template>
+  <Link
+    :href="route('search', {'q': '_all'})"
+    class="hover:text-white/70"
+  >
+    Jobs
+  </Link>
+  <Link
+    :href="route('homepage')"
+    class="hover:text-white/70"
+  >
+    Blog
+  </Link>
+</template>
